99fb4937214313c6a330e798d77e30854af16a4d
edenpaulocruz <edenpaulo@gmail.com>
2021-07-03 17:51:03 -0300

    Permite adicionar tag nos conteúdos

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -6,8 +6,7 @@ class ContentsController < ApplicationController
     @contents = current_user.contents
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @content = Content.new
@@ -17,17 +16,20 @@ class ContentsController < ApplicationController
     @content = current_user.contents.build(content_params)
 
     if @content.save
+      link_tags!
+      
       redirect_to contents_path, notice: 'Content sucessfully created!'
     else
       render :new
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @content.update(content_params)
+      link_tags!
+
       redirect_to contents_path, notice: 'Content sucessfully updated!'
     else
       render :edit
@@ -46,7 +48,19 @@ class ContentsController < ApplicationController
     @content = Content.find(params[:id])
   end
 
+  def tags_params
+    params.require(:content).permit(tags: [])[:tags].reject(&:blank?)
+  end
+
   def content_params
     params.require(:content).permit(:title, :description)
+  end
+
+  def link_tags!
+    tags = tags_params.map do |tag_name|
+      current_user.tags.where(name: tag_name).first_or_initialize
+    end
+
+    @content.tags = tags
   end
 end
