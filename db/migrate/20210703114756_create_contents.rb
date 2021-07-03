7fe39128350322e5f1f0d820961869e3061359e7
edenpaulocruz <edenpaulo@gmail.com>
2021-07-03 09:14:38 -0300

    Adiciona modelo Tag
    Extra: Adiciona has_many :contents no modelo User

--- db/migrate/20210703114756_create_contents.rb
+++ db/migrate/20210703114756_create_contents.rb
@@ -1,8 +1,8 @@
 class CreateContents < ActiveRecord::Migration[6.1]
   def change
     create_table :contents do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
